c614e1fe930b37d40d00670479afeffe61275cc5
Mauricio Stand <mau.stand@gmail.com>
2024-12-26 00:02:18 -0300

    - back button on show movie and serie

--- src/features/movies/showMovie/ShowMovie.tsx
+++ src/features/movies/showMovie/ShowMovie.tsx
@@ -2,8 +2,9 @@ import { MovieDetails } from "@/core/models/movies";
 import MediaOverView from "@/shared/components/media-overview/mediaOverview";
 import OnLoading from "@/shared/components/onLoading/OnLoading";
 import moviesService from "@core/services/api/movies.service";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 
 export default function ShowMovie() {
   const { movieId } = useParams();
@@ -30,13 +31,27 @@ export default function ShowMovie() {
           <h1 className="text-4xl">MOVIE NOT FOUND</h1>
         </div>
       ) : (
+        <>
+          <div className="flex max-w-4xl mx-auto p-4">
+            <h1 className="text-slate-100 text-2xl">Movie</h1>
 
-        <MediaOverView id={movie.id}  
-        name={movie.title} posterPath={movie.poster_path} releaseDate={movie.release_date} 
-        overview={movie.overview}
-        rating={movie.vote_average}
-        genres={movie.genres?.map((i) => i.name)}
-        />
+            <Link
+              className="btn btn-ghost p-2 focus:outline-none ml-auto"
+              to={"../"}
+            >
+              <ArrowUturnLeftIcon className="size-6" />
+            </Link>
+          </div>
+          <MediaOverView
+            id={movie.id}
+            name={movie.title}
+            posterPath={movie.poster_path}
+            releaseDate={movie.release_date}
+            overview={movie.overview}
+            rating={movie.vote_average}
+            genres={movie.genres?.map((i) => i.name)}
+          />
+        </>
       )}
     </>
   );
--- src/features/series/showSerie/ShowSerie.tsx
+++ src/features/series/showSerie/ShowSerie.tsx
@@ -2,8 +2,9 @@ import { SerieDetails } from "@/core/models/series";
 import MediaOverView from "@/shared/components/media-overview/mediaOverview";
 import OnLoading from "@/shared/components/onLoading/OnLoading";
 import seriesServices from "@core/services/api/series.service";
+import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 
 export default function ShowSerie() {
   const { serieId } = useParams();
@@ -30,15 +31,25 @@ export default function ShowSerie() {
           <h1 className="text-4xl">Serie NOT FOUND</h1>
         </div>
       ) : (
-        <MediaOverView
-          id={serie.id}
-          name={serie.name}
-          posterPath={serie.poster_path}
-          releaseDate={serie.first_air_date}
-          overview={serie.overview}
-          rating={serie.vote_average}
-          genres={serie.genres?.map((i) => i.name)}
-        />
+        <>
+          <div className="flex max-w-4xl mx-auto p-4">
+            <h1 className="text-slate-100 text-2xl">Serie</h1>
+
+            <Link  className="btn btn-ghost p-2 focus:outline-none ml-auto" to={"../"}>
+            <ArrowUturnLeftIcon className="size-6" />
+            </Link>
+          </div>
+
+          <MediaOverView
+            id={serie.id}
+            name={serie.name}
+            posterPath={serie.poster_path}
+            releaseDate={serie.first_air_date}
+            overview={serie.overview}
+            rating={serie.vote_average}
+            genres={serie.genres?.map((i) => i.name)}
+          />
+        </>
       )}
     </>
   );
